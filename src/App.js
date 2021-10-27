f04580dc6385115f0831f61c9227550b512702c6
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-10-27 15:18:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Formtable from './components/Formtable';
 import Provider from './Store/Provider';
-// import Users from './Fusers'
 function App() {
   return (
     <Provider>
-    {/* // <Users/> */}
     <Formtable/>
     </Provider>
   );
